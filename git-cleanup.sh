0718a18b6a9902e8f7855feb74b6313b8a247ce2
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-03-16 22:26:46 -0700

    git-cleanup: - Add logs

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+
+echo "Pruning remote branches..."
 git remote prune origin
+echo "Pulling latest from mastser..."
+git pull origin master
+echo "Removing merged local branches..."
 git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d
+
+
