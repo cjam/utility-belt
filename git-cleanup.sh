3f170db03daf613a05bb77350101585701879c8b
Colter McQuay <colter.mcquay@gmail.com>
2019-12-04 10:11:44 -0500

    Updating git-cleanup to work with squash commits

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-echo "Pruning remote branches..."
-git remote prune origin
+# Modified to work with squash commits
+# https://medium.com/opendoor-labs/cleaning-up-branches-with-githubs-squash-merge-43138cc7585e
+
+echo "Switching to master..."
+git checkout master
 echo "Pulling latest from mastser..."
 git pull origin master
-echo "Removing merged local branches..."
-git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d
-
-
+echo "Deleting orphaned local branches"
+comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D %
