f03bc7c2596e40bd560206133389b1c1da82aac0
Colter McQuay <colter.mcquay@gmail.com>
2020-03-25 10:57:22 -0700

    Linux compatibility

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 
 # Modified to work with squash commits
 # https://medium.com/opendoor-labs/cleaning-up-branches-with-githubs-squash-merge-43138cc7585e
@@ -9,4 +17,8 @@ git checkout master
 echo "Pulling latest from mastser..."
 git pull origin master
 echo "Deleting orphaned local branches"
-comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D %
+if [ "$machine" == "Mac" ];then
+    comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D %
+elif [ "$machine" == "Linux" ];then
+    comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -i % git branch -D %
+fi
