f6cf3696baf8542e0346194cbe3f29a39b24c1bf
Colter McQuay <colter.mcquay@gmail.com>
2020-12-16 12:43:11 -0800

    Add support for main/master branch

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -4,9 +4,16 @@
 # Modified to work with squash commits
 # https://medium.com/opendoor-labs/cleaning-up-branches-with-githubs-squash-merge-43138cc7585e
 
-echo "Switching to master..."
-git checkout master
-echo "Pulling latest from mastser..."
-git pull origin master
+MAIN_BRANCH="master"
+
+echo "Switching to $MAIN_BRANCH..."
+if ! git checkout $MAIN_BRANCH ; then
+  echo "Error checking out $MAIN_BRANCH"
+  MAIN_BRANCH="main"
+  echo "Switching to $MAIN_BRANCH..."
+  git checkout $MAIN_BRANCH
+fi
+echo "Pulling latest from $MAIN_BRANCH..."
+git pull origin $MAIN_BRANCH
 echo "Deleting orphaned local branches"
 comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D %
